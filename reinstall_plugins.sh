9b7919a5e55e08e9516d91b728f9a4f67e96fc7c
Kunio SAWATANI <devel@fathens.org>
2014-08-22 09:27:58 +0900

    GoogleMaps を GoogleConnect の後にインストール
    GoogleConnect で GooglePlayServices の最新版をインストールするので、
    その後で GoogleMaps をインストールするようにしました。

--- reinstall_plugins.sh
+++ reinstall_plugins.sh
@@ -15,9 +15,6 @@ cordova plugin add org.apache.cordova.geolocation
 # Ionic Keyboard
 cordova plugin add https://github.com/driftyco/ionic-plugins-keyboard.git
 
-# Google Maps
-cordova plugin add plugin.google.maps --variable API_KEY_FOR_IOS='AIzaSyBbBpBdB0rQQPsvd1Xv1QW_vftXxsgDYr4' --variable API_KEY_FOR_ANDROID='AIzaSyDYNGDBMUgZsnAzFZ4-uZB4VQuSxEfe1Lg'
-
 # Google+ connect
 cordova plugin add https://github.com/sawatani/Cordova-GooglePlus.git --variable IOS_CLIENT_ID='945048561360-k8admbvmnc16qkj69s5f6kj154holr7p.apps.googleusercontent.com'
 
@@ -26,5 +23,9 @@ cordova plugin add https://github.com/sawatani/phonegap-facebook-plugin.git --va
 (cd platforms/android/ && android update project -p . -l FacebookLib) # Add as library
 (cd platforms/android/FacebookLib && android update lib-project -p . && ant instrument) # Make gen
 
+# Google Maps
+cordova plugin add plugin.google.maps --variable API_KEY_FOR_IOS='AIzaSyBbBpBdB0rQQPsvd1Xv1QW_vftXxsgDYr4' --variable API_KEY_FOR_ANDROID='AIzaSyDYNGDBMUgZsnAzFZ4-uZB4VQuSxEfe1Lg'
+
 # Remove confliction with platforms/android/FacebookLib/libs/android-support-v4.jar
-(cd platforms/android && [ -e libs/android-support-v4.jar ] && cp -vf FacebookLib/libs/android-support-v4.jar libs/) # Resolve confliction
+(cd platforms/android && [ -e libs/android-support-v4.jar ] && cp -vf FacebookLib/libs/android-support-v4.jar libs/)
+
