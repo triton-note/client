c519c5f938c422d91e8733295dededc595b47422
Kunio SAWATANI <devel@fathens.org>
2014-12-10 17:25:02 +0900

    iOS の CFBundleURLSchemes は Facebook を優先
    plugin.google.maps が CFBundleURLSchemes を先にセットしてしまうと phonegap-facebook-plugin がセット出来なくなるので、順番を入れ替えました。

--- reinstall_plugins.sh
+++ reinstall_plugins.sh
@@ -12,11 +12,11 @@ cordova plugin add org.apache.cordova.splashscreen
 
 cordova plugin add org.apache.cordova.file-transfer
 
+# Facebook connect
+cordova -d plugin add https://github.com/Wizcorp/phonegap-facebook-plugin.git --variable APP_ID="751407064903894" --variable APP_NAME="TritonNote"
+
 # Google Maps
 cordova plugin add plugin.google.maps --variable API_KEY_FOR_IOS='AIzaSyBbBpBdB0rQQPsvd1Xv1QW_vftXxsgDYr4' --variable API_KEY_FOR_ANDROID='AIzaSyDYNGDBMUgZsnAzFZ4-uZB4VQuSxEfe1Lg'
-
-# Facebook connect
-cordova plugin add https://github.com/Wizcorp/phonegap-facebook-plugin.git --variable APP_ID="751407064903894" --variable APP_NAME="TritonNote"
 
 # Aid for bug of Http
 cordova plugin add https://github.com/sawatani/Cordova-plugin-okhttp.git
