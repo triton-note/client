9e4b619e042ea13e2abb806a7a7ab3223023844b
Kunio Sawatani <devel@fathens.org>
2015-02-02 17:08:52 +0900

    Add Wercker Settings
    ハードコーディングしてあった値を環境変数に切り替えました。

--- reinstall_plugins.sh
+++ reinstall_plugins.sh
@@ -11,9 +11,9 @@ cordova plugin add org.apache.cordova.camera
 cordova plugin add org.apache.cordova.splashscreen
 
 # Facebook connect
-cordova plugin add https://github.com/Wizcorp/phonegap-facebook-plugin.git --variable APP_ID="751407064903894" --variable APP_NAME="TritonNote"
+cordova plugin add https://github.com/Wizcorp/phonegap-facebook-plugin.git --variable APP_ID="$FACEBOOK_APP_ID" --variable APP_NAME="$FACEBOOK_APP_NAME"
 # Google Maps
-cordova plugin add plugin.google.maps --variable API_KEY_FOR_IOS='AIzaSyBbBpBdB0rQQPsvd1Xv1QW_vftXxsgDYr4' --variable API_KEY_FOR_ANDROID='AIzaSyDYNGDBMUgZsnAzFZ4-uZB4VQuSxEfe1Lg'
+cordova plugin add plugin.google.maps --variable API_KEY_FOR_IOS="$GOOGLE_API_KEY_FOR_IOS" --variable API_KEY_FOR_ANDROID="$GOOGLE_API_KEY_FOR_ANDROID"
 # Merging support-v4 from Facebook and GoogleMaps dependency
 SUPPORT_V4_FB=platforms/android/com.phonegap.plugins.facebookconnect/*FacebookLib/libs/android-support-v4.jar
 SUPPORT_V4=platforms/android/libs/android-support-v4.jar
@@ -22,4 +22,5 @@ SUPPORT_V4=platforms/android/libs/android-support-v4.jar
 # Aid for bug of Http
 cordova plugin add https://github.com/sawatani/Cordova-plugin-okhttp.git
 # Crash Report
-cordova plugin add https://github.com/sawatani/Cordova-plugin-acra.git --variable MAIL_TO='devel+triton_note-crash@fathens.org' --variable TOAST_TEXT='Crash Report Sent'
+cordova plugin add https://github.com/sawatani/Cordova-plugin-acra.git --variable MAIL_TO="devel+$ACRA_APP_NAME-crash@fathens.org" --variable TOAST_TEXT='Crash Report Sent'
+cat platforms/android/AndroidManifest.xml | awk '/<application/ { sub(">", " android:name=\"org.fathens.cordova.acra.AcraApplication\">"); print $0} !/<application/ { print $0 }'
