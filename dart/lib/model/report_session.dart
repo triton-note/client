24182642eb87b0a3ad0c4553003979315a28b507
Kunio Sawatani <devel@fathens.org>
2015-04-22 18:26:52 +0900

    SessionToken は不変にした

--- dart/lib/model/report_session.dart
+++ dart/lib/model/report_session.dart
@@ -1,5 +1,7 @@
 library report_session;
 
+import 'dart:collection';
+
 import 'package:triton_note/model/report.dart';
 
 class SessionInference {
@@ -14,6 +16,6 @@ class SessionToken {
   final String uploadUrl;
   final Map<String, String> uploadParams;
 
-  SessionToken(this.token, this.uploadUrl, this.uploadParams);
+  SessionToken(this.token, this.uploadUrl, Map<String, String> map): uploadParams = new UnmodifiableMapView(map);
 }
 
