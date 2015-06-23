112204437a97342311408977c70824acfe4d088e
Kunio Sawatani <devel@fathens.org>
2015-06-23 13:53:52 +0900

    FileChooser を連続で呼ばれる場合に対応

--- dart/lib/util/dialog.dart
+++ dart/lib/util/dialog.dart
@@ -5,25 +5,34 @@ import 'dart:html';
 
 import 'package:paper_elements/paper_dialog.dart';
 
-Future<File> chooseFile() async {
-  final html = """
-<paper-dialog id="fileChooser" heading="Choose file" transition="core-transition-center" backdrop autoCloseDisabled>
-  <input type="file" id="fileInput"></input>
-</paper-dialog>
-""";
-  document.body.appendHtml(html);
+Future<File> chooseFile() {
   final result = new Completer<File>();
 
-  final fileChooser = document.querySelector("#fileChooser") as PaperDialog;
-  final fileInput = document.querySelector("#fileInput") as InputElement;
-  fileInput.onChange.listen((event) {
+  final fileChooser = _appendDialog();
+  final fileInput = fileChooser.querySelector("#fileInput") as InputElement;
+  final sub = fileInput.onChange.listen(null);
+  sub.onData((event) {
     final files = fileInput.files;
     if (files.length > 0) {
       result.complete(files[0]);
       fileChooser.toggle();
+      sub.cancel();
     }
   });
   print("Toggle dialog: ${fileChooser}");
   fileChooser.toggle();
   return result.future;
+}
+
+PaperDialog _appendDialog() {
+  fileChooser() => document.querySelector("#fileChooser");
+  final e = fileChooser();
+  if (e != null) return e;
+
+  document.body.appendHtml("""
+<paper-dialog id="fileChooser" heading="Choose file" transition="core-transition-center" backdrop autoCloseDisabled>
+  <input type="file" id="fileInput"></input>
+</paper-dialog>
+""");
+  return fileChooser();
 }
