7cc78008c044595841cffecadc934fea3dde26cb
Kunio Sawatani <devel@fathens.org>
2015-06-25 12:09:54 +0900

    カレンダーのページめくりを実装

--- dart/lib/element/calendar.dart
+++ dart/lib/element/calendar.dart
@@ -10,29 +10,86 @@ import 'package:core_elements/core_animated_pages.dart';
     cssUrl: 'packages/triton_note/element/calendar.css',
     useShadowDom: true)
 class CalendarElement extends ShadowRootAware {
+  static const maxPages = 10;
+  static const transitionNormal = "slide-from-right";
+  static const transitionToday = "cross-fade-all";
+
   @NgTwoWay('value') DateTime value;
 
   CoreAnimatedPages _pages;
-  int _currentMonth;
+  DateTime _currentFirst;
+  get currentFirst => _currentFirst;
+  set currentFirst(DateTime v) => _currentFirst = new DateTime(v.year, v.month);
 
   void onShadowRoot(ShadowRoot shadowRoot) {
     _pages = shadowRoot.getElementById('calendar-pages');
-    _currentMonth = value.month;
-    _makePage(_currentMonth);
+    currentFirst = new DateTime(value.year, value.month);
+    _pages.append(_makePage());
     _pages.selected = 0;
   }
 
-  _makePage(int month) {
+  Element _makePage() {
+    print("Creating page: ${currentFirst}");
+    final format = new Date();
     final page = document.createElement('section');
-    final content = document.createElement('paper-shadow');
+    final content = document.createElement('div');
     content.appendHtml("""
-<div><p>${month}</p></div>
+<div id="head"><p>${format(currentFirst, 'MMM, yyyy')}</p></div>
 """);
     page.append(content);
-    _pages.append(page);
+    return page;
   }
 
-  previousMonth() {}
+  today() async {
+    currentFirst = new DateTime.now();
+    print("Today's month: ${currentFirst}");
+    _pages.transitions = transitionToday;
 
-  nextMonth() {}
+    final all = _pages.querySelectorAll('section');
+    print("Remove all sections: ${all.length}");
+    all.forEach((e) => e.remove());
+
+    _pages.append(_makePage());
+    _pages.selected = 0;
+  }
+
+  previousMonth() async {
+    currentFirst = currentFirst.subtract(new Duration(days: 1));
+    print("Previous month: ${currentFirst}");
+    _pages.transitions = transitionNormal;
+
+    final index = _pages.selected;
+    if (index == 0) {
+      final all = _pages.querySelectorAll('section');
+      print("sections: ${all.length}");
+      if (maxPages < all.length) {
+        all.last.remove();
+      }
+
+      _pages.insertBefore(_makePage(), all.first);
+      _pages.selected = index;
+    } else {
+      _pages.selected = index - 1;
+    }
+  }
+
+  nextMonth() async {
+    currentFirst = currentFirst.add(new Duration(days: 31));
+    print("Next month: ${currentFirst}");
+    _pages.transitions = transitionNormal;
+
+    final index = _pages.selected;
+    final all = _pages.querySelectorAll('section');
+    print("sections: ${all.length}");
+
+    if (index == all.length - 1) {
+      _pages.append(_makePage());
+    }
+    if (maxPages < all.length) {
+      all.first.remove();
+      _pages.selected = index;
+    } else {
+      _pages.selected = index + 1;
+    }
+  }
 }
