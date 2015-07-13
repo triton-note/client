81640efa902a0ea09970922fc6d88112a5d7b039
Kunio Sawatani <devel@fathens.org>
2015-07-13 20:35:19 +0900

    catches を編集できるように変更
    report_detail 内で、edit-fish-dialog を使って編集できるようにしました。

--- dart/lib/page/report_detail.dart
+++ dart/lib/page/report_detail.dart
@@ -10,12 +10,14 @@ import 'package:core_elements/core_animated_pages.dart';
 import 'package:core_elements/core_header_panel.dart';
 import 'package:paper_elements/paper_icon_button.dart';
 
+import 'package:triton_note/dialog/edit_fish.dart';
 import 'package:triton_note/dialog/edit_timestamp.dart';
 import 'package:triton_note/model/report.dart';
 import 'package:triton_note/model/location.dart';
 import 'package:triton_note/model/value_unit.dart';
 import 'package:triton_note/service/preferences.dart';
 import 'package:triton_note/service/reports.dart';
+import 'package:triton_note/service/server.dart';
 import 'package:triton_note/service/googlemaps_browser.dart';
 import 'package:triton_note/util/enums.dart';
 import 'package:triton_note/util/getter_setter.dart';
@@ -34,10 +36,12 @@ const String editFlop = "done";
 class ReportDetailPage extends MainFrame {
   Future<Report> _report;
   Report report;
+  _Catches catches;
   _PhotoSize photo;
   _GMap gmap;
   _Conditions conditions;
   GetterSetter<EditTimestampDialog> editTimestamp = new PipeValue();
+  Timer submitTimer;
 
   ReportDetailPage(Router router, RouteProvider routeProvider) : super(router) {
     final String reportId = routeProvider.parameters['reportId'];
@@ -52,10 +56,108 @@ class ReportDetailPage extends MainFrame {
 
     _report.then((v) async {
       report = v;
+      catches = new _Catches(root, new GetterSetter(() => report.fishes, (v) => report.fishes = v));
       conditions = new _Conditions(report.condition);
       gmap = new _GMap(root, report.location.geoinfo);
     });
   }
+
+  void onChanged() {
+    if (submitTimer != null && submitTimer.isActive) submitTimer.cancel();
+    submitTimer = new Timer(new Duration(seconds: 30), () {
+      Server.update(report);
+    });
+  }
+}
+
+class _Catches {
+  static const buttonsFrame = const [const {'opacity': 0.05}, const {'opacity': 1}];
+  static const itemFrame = const [const {'background': "#fffcfc"}, const {'background': "#fee"}];
+  static const Duration blinkDuration = const Duration(seconds: 2);
+  static const Duration blinkDownDuration = const Duration(milliseconds: 300);
+
+  final ShadowRoot _root;
+  final GetterSetter<List<Fishes>> list;
+  GetterSetter<EditFishDialog> dialog = new PipeValue();
+  bool isEditing = false;
+  Timer _blinkTimer;
+  List<CoreAnimation> _animations;
+
+  _Catches(this._root, this.list);
+
+  toggle(event) {
+    final button = event.target as PaperIconButton;
+    _logger.fine("Toggle edit: ${button.icon}");
+    button.icon = isEditing ? editFlip : editFlop;
+
+    final addButton = new CachedValue(() => _root.querySelector('#fishes paper-icon-button.add'));
+    final fishItems = new CachedValue(() => _root.querySelectorAll('#fishes div.item'));
+
+    blink(final bool updown, [final duration = blinkDuration, final repeat = true]) {
+      _animations = [];
+
+      animate(Element target, List frames) {
+        final keyframes = updown ? frames : frames.reversed.toList();
+        _logger.finest("Blink: ${target}: ${keyframes}");
+        final a = new CoreAnimation()
+          ..target = target
+          ..duration = duration.inMilliseconds
+          ..keyframes = keyframes
+          ..fill = "forwards"
+          ..easing = "ease-in-out"
+          ..play();
+        _animations.add(a);
+      }
+      animate(addButton.value, buttonsFrame);
+      fishItems.value.forEach((item) {
+        animate(item, itemFrame);
+      });
+      if (repeat) _blinkTimer = new Timer(blinkDuration, () => blink(!updown));
+    }
+
+    if (isEditing) {
+      _logger.finest("Blink stopping...");
+      if (_blinkTimer != null && _blinkTimer.isActive) _blinkTimer.cancel();
+      if (_animations != null) _animations.forEach((a) => a.cancel());
+      blink(false, blinkDownDuration, false);
+      new Future.delayed(blinkDownDuration, () {
+        isEditing = false;
+      });
+    } else {
+      isEditing = true;
+      new Future.delayed(new Duration(milliseconds: 10), () {
+        blink(true);
+
+        fishItems.value.forEach((item) {
+          item.querySelector('paper-ripple').style
+            ..position = "absolute"
+            ..top = '0'
+            ..bottom = '0'
+            ..left = '0'
+            ..right = '0';
+        });
+      });
+    }
+  }
+
+  add() => alfterRippling(() {
+    _logger.fine("Add new fish");
+    final fish = new Fishes.fromMap({'count': 1});
+    dialog.value.open(new GetterSetter(() => fish, (v) {
+      list.value = list.value..add(v);
+    }));
+  });
+
+  edit(index) => alfterRippling(() {
+    _logger.fine("Edit at $index");
+    dialog.value.open(new GetterSetter(() => list.value[index], (v) {
+      if (v == null) {
+        list.value = list.value..removeAt(index);
+      } else {
+        list.value = list.value..[index] = v;
+      }
+    }));
+  });
 }
 
 class _GMap {
