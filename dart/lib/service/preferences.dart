4972a2b5712c69f8c536df7bd2c4149159a63d46
Kunio Sawatani <devel@fathens.org>
2015-07-15 14:18:33 +0900

    Server から２重にロードしないように改修

--- dart/lib/service/preferences.dart
+++ dart/lib/service/preferences.dart
@@ -10,9 +10,14 @@ import 'package:triton_note/service/server.dart';
 final _logger = new Logger('UserPreferences');
 
 class UserPreferences {
-  static Measures _measures;
-  static Future<Measures> get measures async =>
-      (_measures != null) ? _measures : Server.loadMeasures().then((v) => _measures = v);
+  static Future<Measures> _measures;
+  static Future<Measures> get measures {
+    if (_measures == null) _measures = Server.loadMeasures();
+    return _measures;
+  }
 
-  static update(Measures v) => Server.updateMeasures(_measures = v);
+  static update(Measures v) {
+    _measures = new Future.value(v);
+    Server.updateMeasures(v);
+  }
 }
