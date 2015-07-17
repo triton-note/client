ff15607d4e544d9406223cb83e4776abcdf72a0e
Kunio Sawatani <devel@fathens.org>
2015-07-17 15:05:55 +0900

    util.enums で null に対応

--- dart/lib/util/enums.dart
+++ dart/lib/util/enums.dart
@@ -1,4 +1,4 @@
 library triton_note.util.enums;
 
-String nameOfEnum(e) => e.toString().split("\.").last;
-enumByName(List values, String name) => values.firstWhere((a) => nameOfEnum(a) == name);
+String nameOfEnum(e) => e == null ? null : e.toString().split("\.").last;
+enumByName(List values, String name) => name == null ? null : values.firstWhere((a) => nameOfEnum(a) == name);
