18728edc049f06f43de28bc39a0f5be2895fbd86
Kunio Sawatani <devel@fathens.org>
2015-07-21 22:47:00 +0900

    distributions-filter 内を細分化

--- dart/lib/element/distributions_filter.dart
+++ dart/lib/element/distributions_filter.dart
@@ -1,17 +1,23 @@
 library triton_note.element.distributions_filter;
 
+import 'dart:async';
 import 'dart:html';
 
 import 'package:angular/angular.dart';
 import 'package:logging/logging.dart';
 import 'package:paper_elements/paper_radio_group.dart';
 
+import 'package:triton_note/model/location.dart';
 import 'package:triton_note/dialog/edit_timestamp.dart';
+import 'package:triton_note/dialog/edit_weather.dart';
+import 'package:triton_note/dialog/edit_tide.dart';
 import 'package:triton_note/service/preferences.dart';
 import 'package:triton_note/util/getter_setter.dart';
 import 'package:triton_note/util/enums.dart';
 
 final _logger = new Logger('DistributionsFilterElement');
+
+const _selectDur = const Duration(milliseconds: 10);
 
 @Component(
     selector: 'distributions-filter',
@@ -22,34 +28,85 @@ class DistributionsFilterElement extends ShadowRootAware {
   @NgOneWay('setter') Setter<DistributionsFilterElement> setter;
 
   ShadowRoot _root;
+  _FishKind kind;
+  _FishSize size;
+  _Conditions cond;
+  _Term term;
 
   void onShadowRoot(ShadowRoot sr) {
     _root = sr;
     setter.value = this;
 
-    _root.querySelector('#term paper-radio-group').on['core-select'].listen((event) {
-      final radio = event.target;
-      if (radio is PaperRadioGroup) {
-        _logger.finest("Term radio selected: ${radio.selected}");
-      }
-    });
+    kind = new _FishKind(_root);
+    size = new _FishSize(_root);
+    cond = new _Conditions(_root);
+    term = new _Term(_root);
   }
+}
+
+enum _RecentUnit { days, weeks, months }
+
+class _FishKind {
+  final ShadowRoot _root;
+
+  String name;
+
+  _FishKind(this._root);
+}
+
+class _FishSize {
+  final ShadowRoot _root;
+
+  _FishSize(this._root);
 
   String get lengthUnit => nameOfEnum(CachedMeasures.lengthUnit);
   String get weightUnit => nameOfEnum(CachedMeasures.weightUnit);
 
-  // size
-  int sizeMinLength = 0;
-  bool get sizeMinLengthActive => sizeMinLength > 0;
-  int sizeMaxLength = 0;
-  bool get sizeMaxLengthActive => sizeMaxLength > sizeMinLength;
-  bool get sizeLengthActive => sizeMinLengthActive || sizeMaxLengthActive;
+  int lengthMin = 0;
+  bool get lengthMinActive => lengthMin > 0;
+  int lengthMax = 0;
+  bool get lengthMaxActive => lengthMax > lengthMin;
+  bool get lengthActive => lengthMinActive || lengthMaxActive;
 
-  int sizeMinWeight = 0;
-  bool get sizeMinWeightActive => sizeMinWeight > 0;
-  int sizeMaxWeight = 0;
-  bool get sizeMaxWeightActive => sizeMaxWeight > sizeMinWeight;
-  bool get sizeWeightActive => sizeMinWeightActive || sizeMaxWeightActive;
+  int weightMin = 0;
+  bool get weightMinActive => weightMin > 0;
+  int weightMax = 0;
+  bool get weightMaxActive => weightMax > weightMin;
+  bool get weightActive => weightMinActive || weightMaxActive;
+}
+
+class _Conditions {
+  final ShadowRoot _root;
+
+  Weather weather;
+  int temperatureMin, temperatureMax;
+
+  Tide tide;
+  String get tideName => nameOfEnum(tide);
+  String get tideIcon => Tides.iconOf(tide);
+
+  int moon;
+  Getter<EditWeatherDialog> weatherDialog = new PipeValue();
+  Getter<EditTideDialog> tideDialog = new PipeValue();
+
+  _Conditions(this._root);
+}
+
+class _Term {
+  final ShadowRoot _root;
+
+  _Term(this._root) {
+    Timer timer;
+    _root.querySelector('#term paper-radio-group').on['core-select'].listen((event) {
+      final radio = event.target;
+      if (radio is PaperRadioGroup) {
+        if (timer != null && timer.isActive) timer.cancel();
+        timer = new Timer(_selectDur, () {
+          _logger.finest("Term radio selected: ${radio.selected}");
+        });
+      }
+    });
+  }
 
   int recentValue;
   String recentUnitName = nameOfEnum(_RecentUnit.values.first);
@@ -59,10 +116,8 @@ class DistributionsFilterElement extends ShadowRootAware {
   int seasonBegin = 1;
   int seasonEnd = 2;
 
-  DateTime termFrom = new DateTime.now();
-  Getter<EditTimestampDialog> termFromDialog = new PipeValue();
-  DateTime termTo = new DateTime.now();
-  Getter<EditTimestampDialog> termToDialog = new PipeValue();
+  DateTime intervalFrom = new DateTime.now();
+  Getter<EditTimestampDialog> intervalFromDialog = new PipeValue();
+  DateTime intervalTo = new DateTime.now();
+  Getter<EditTimestampDialog> intervalToDialog = new PipeValue();
 }
-
-enum _RecentUnit { days, weeks, months }
