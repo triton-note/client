69c1aa0cfed630c3d4ab1cab7c4abeeba7373b23
Kunio Sawatani <devel@fathens.org>
2015-08-06 15:24:49 +0900

    FileChooser を index.html に予め用意
    Chromium を最新にするとなぜか paper-dialog を挿入できなくなったので止む得ずこうします。

--- dart/lib/util/dialog.dart
+++ dart/lib/util/dialog.dart
@@ -11,32 +11,22 @@ final _logger = new Logger('Dialog');
 
 Future<File> chooseFile() {
   final result = new Completer<File>();
-
-  final fileChooser = _appendDialog();
-  final fileInput = fileChooser.querySelector("#fileInput") as InputElement;
-  final sub = fileInput.onChange.listen(null);
-  sub.onData((event) {
-    final files = fileInput.files;
-    if (files.length > 0) {
-      result.complete(files[0]);
-      fileChooser.toggle();
-      sub.cancel();
-    }
-  });
-  _logger.fine("Toggle dialog: ${fileChooser}");
-  fileChooser.toggle();
+  try {
+    final fileChooser = document.querySelector("div#options #fileChooser") as PaperDialog;
+    final fileInput = fileChooser.querySelector("#fileInput") as InputElement;
+    final sub = fileInput.onChange.listen(null);
+    sub.onData((event) {
+      final files = fileInput.files;
+      if (files.length > 0) {
+        result.complete(files[0]);
+        fileChooser.toggle();
+        sub.cancel();
+      }
+    });
+    _logger.fine("Toggle dialog: ${fileChooser}");
+    fileChooser.toggle();
+  } catch (ex) {
+    result.completeError(ex);
+  }
   return result.future;
-}
-
-PaperDialog _appendDialog() {
-  fileChooser() => document.querySelector("#fileChooser");
-  final e = fileChooser();
-  if (e != null) return e;
-
-  document.body.appendHtml("""
-<paper-dialog id="fileChooser" heading="Choose file" transition="core-transition-center" backdrop autoCloseDisabled>
-  <input type="file" id="fileInput"></input>
-</paper-dialog>
-""");
-  return fileChooser();
 }
