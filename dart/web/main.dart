41e09c410196573c818c29b721e43e6b6db324f8
Kunio Sawatani <devel@fathens.org>
2015-08-11 15:28:17 +0900

    TideFormater を作成

--- dart/web/main.dart
+++ dart/web/main.dart
@@ -5,6 +5,7 @@ import 'dart:html';
 import 'package:triton_note/router.dart';
 import 'package:triton_note/formatter/fish_formatter.dart';
 import 'package:triton_note/formatter/temperature_formatter.dart';
+import 'package:triton_note/formatter/tide_formatter.dart';
 import 'package:triton_note/dialog/edit_fish.dart';
 import 'package:triton_note/dialog/edit_timestamp.dart';
 import 'package:triton_note/dialog/edit_tide.dart';
@@ -34,6 +35,7 @@ class AppModule extends Module {
   AppModule() {
     bind(FishFormatter);
     bind(TemperatureFormatter);
+    bind(TideFormatter);
 
     bind(EditFishDialog);
     bind(EditTimestampDialog);
