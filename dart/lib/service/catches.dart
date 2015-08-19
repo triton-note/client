39de197911e1f9d11b4644863b60a823574c5e10
Kunio Sawatani <devel@fathens.org>
2015-08-19 14:49:22 +0900

    CatchesPager を実装

--- dart/lib/service/catches.dart
+++ dart/lib/service/catches.dart
@@ -46,3 +46,49 @@ class Catches {
   @override
   String toString() => "Catches(${dateAt}, ${location}, ${condition}, ${fish})";
 }
+
+class CatchesPager {
+  static const int pageSize = 100;
+
+  final PagingDB<Report> _pager;
+  final DistributionsFilter filter;
+
+  CatchesPager(this._pager, this.filter);
+
+  bool get hasMore => _pager.hasMore;
+
+  List<Fishes> _left = [];
+
+  Future<List<Fishes>> more() async {
+    Future<List<Fishes>> doMore() async {
+      final reports = await _pager.more(pageSize);
+      final list = await Future.wait(reports.map((report) async {
+        await Reports.loadFishes(report);
+        return report.fishes.where((fish) {
+          if (filter.fish.isActiveName) {
+            if (!fish.name.contains(filter.fish.name)) return false;
+          }
+          if (filter.fish.isActiveLength) {
+            final value = fish.length.asStandard().value;
+            if (filter.fish.isActiveLengthMin && value < filter.fish.lengthMin) return false;
+            if (filter.fish.isActiveLengthMax && value > filter.fish.lengthMax) return false;
+          }
+          if (filter.fish.isActiveWeight) {
+            final value = fish.weight.asStandard().value;
+            if (filter.fish.isActiveWeightMin && value < filter.fish.weightMin) return false;
+            if (filter.fish.isActiveWeightMax && value > filter.fish.weightMax) return false;
+          }
+          return true;
+        }).map((fish) => new Catches(fish, report.dateAt, report.location, report.condition));
+      }));
+      return list.expand((a) => a).toList();
+    }
+
+    while (_left.length < pageSize && _pager.hasMore) {
+      _left.addAll(await doMore());
+    }
+    final result = _left.take(pageSize);
+    _left = _left.sublist(result.length);
+    return result.toList();
+  }
+}
