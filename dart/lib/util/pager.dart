2c985ae4c68c31306013daf2cb5f5503eb39d990
Kunio Sawatani <devel@fathens.org>
2015-08-25 13:46:04 +0900

    Pager を利用した InfiniteList を作成

--- dart/lib/util/pager.dart
+++ dart/lib/util/pager.dart
@@ -1,6 +1,7 @@
 library triton_note.util.pager;
 
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:logging/logging.dart';
 
@@ -10,4 +11,29 @@ abstract class Pager<T> {
   bool get hasMore;
   Future<List<T>> more(int pageSize);
   void reset();
+}
+
+class InfiniteList<T> implements Pager<T> {
+  final Pager<T> _pager;
+  final List<T> _list = [];
+  UnmodifiableListView<T> _view;
+
+  InfiniteList(this._pager);
+
+  List<T> get list => _view;
+
+  bool get hasMore => _pager.hasMore;
+
+  void reset() {
+    _pager.reset();
+    _list.clear();
+    _view = new UnmodifiableListView(_list);
+  }
+
+  Future<List<T>> more(int pageSize) async {
+    final a = await _pager.more(pageSize);
+    _list.addAll(a);
+    _view = new UnmodifiableListView(_list);
+    return a;
+  }
 }
