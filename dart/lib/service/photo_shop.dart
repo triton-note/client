68584b02a7626eeb80c6e62bf22a42d6eca29efe
Kunio Sawatani <devel@fathens.org>
2015-08-31 15:57:37 +0900

    Cordova-plugin-photo を使わないように改修
    パラメータの変更などが面倒なので外部のプラグインにするのはやめました。
    util.fileReader を作成し、FileReader の扱いをここで統一させます。

--- dart/lib/service/photo_shop.dart
+++ dart/lib/service/photo_shop.dart
@@ -10,10 +10,13 @@ import 'package:image/image.dart';
 import 'package:triton_note/model/location.dart';
 import 'package:triton_note/util/cordova.dart';
 import 'package:triton_note/util/dialog.dart' as Dialog;
+import 'package:triton_note/util/file_reader.dart';
 
 final _logger = new Logger('PhotoShop');
 
 class PhotoShop {
+  static const CONTENT_TYPE = 'image/jpeg';
+
   final Completer<Blob> _onChoose = new Completer();
   final Completer<String> _onGetUrl = new Completer();
   final Completer<GeoInfo> _onGetGeoinfo = new Completer();
@@ -47,56 +50,41 @@ class PhotoShop {
     }
   }
 
-  _readExif(Blob blob) {
+  _readExif(Blob blob) async {
+    _logger.fine("Exif Loading on ${blob}");
     try {
-      final reader = new JsObject(context['FileReader'], []);
-      reader['onloadend'] = (event) {
-        try {
-          final array = reader['result'];
-          _logger.fine("Exif Loading on ${array}");
-          final exif = new JsObject(context['ExifReader'], []);
-          exif.callMethod('load', [array]);
+      final array = await fileReader_readAsArrayBuffer(blob.slice(0, 128 * 1024));
 
-          get(String name) => exif.callMethod('getTagDescription', [name]);
+      final exif = new JsObject(context['ExifReader'], []);
+      exif.callMethod('load', [array]);
 
-          try {
-            String text = get('DateTimeOriginal');
-            if (text == null) text = get('DateTimeDigitized');
-            if (text == null) text = get('DateTime');
-            final a = text.split(' ').expand((e) => e.split(':')).map(int.parse).toList();
-            _logger.fine("Exif: Timestamp: ${a}");
-            final date = new DateTime(a[0], a[1], a[2], a[3], a[4], a[5]);
-            _onGetTimestamp.complete(date);
-          } catch (ex) {
-            _logger.fine("Exif: Timestamp: Error: ${ex}");
-            _onGetTimestamp.completeError(ex);
-          }
-          try {
-            final double lat = get('GPSLatitude');
-            final double lon = get('GPSLongitude');
-            _logger.fine("Exif: GPS: latitude=${lat}, longitude=${lon}");
-            if (lat != null && lon != null) {
-              _onGetGeoinfo.complete(new GeoInfo.fromMap({'latitude': lat, 'longitude': lon}));
-            } else {
-              _onGetGeoinfo.completeError("Exif: GPS: Error: null value");
-            }
-          } catch (ex) {
-            _logger.fine("Exif: GPS: Error: ${ex}");
-            _onGetGeoinfo.completeError(ex);
-          }
-        } catch (ex) {
-          _logger.fine("Exif: Error: ${ex}");
-          _onGetTimestamp.completeError(ex);
-          _onGetGeoinfo.completeError(ex);
+      get(String name) => exif.callMethod('getTagDescription', [name]);
+
+      try {
+        String text = get('DateTimeOriginal');
+        if (text == null) text = get('DateTimeDigitized');
+        if (text == null) text = get('DateTime');
+        final a = text.split(' ').expand((e) => e.split(':')).map(int.parse).toList();
+        _logger.fine("Exif: Timestamp: ${a}");
+        final date = new DateTime(a[0], a[1], a[2], a[3], a[4], a[5]);
+        _onGetTimestamp.complete(date);
+      } catch (ex) {
+        _logger.fine("Exif: Timestamp: Error: ${ex}");
+        _onGetTimestamp.completeError(ex);
+      }
+      try {
+        final double lat = get('GPSLatitude');
+        final double lon = get('GPSLongitude');
+        _logger.fine("Exif: GPS: latitude=${lat}, longitude=${lon}");
+        if (lat != null && lon != null) {
+          _onGetGeoinfo.complete(new GeoInfo.fromMap({'latitude': lat, 'longitude': lon}));
+        } else {
+          _onGetGeoinfo.completeError("Exif: GPS: Error: null value");
         }
-      };
-      reader['onerror'] = (event) {
-        final error = reader['error'];
-        _logger.fine("Exif: Error on reading blob: ${error}");
-        _onGetTimestamp.completeError(error);
-        _onGetGeoinfo.completeError(error);
-      };
-      reader.callMethod('readAsArrayBuffer', [blob.slice(0, 128 * 1024)]);
+      } catch (ex) {
+        _logger.fine("Exif: GPS: Error: ${ex}");
+        _onGetGeoinfo.completeError(ex);
+      }
     } catch (ex) {
       _logger.fine("Exif: Error: ${ex}");
       _onGetTimestamp.completeError(ex);
@@ -107,15 +95,30 @@ class PhotoShop {
   _photo(bool take) async {
     try {
       if (isCordova) {
-        context['plugin']['photo'].callMethod(take ? 'take' : 'select', [
-          (blob) {
-            _logger.fine("Get photo: ${blob}");
-            _onChoose.complete(blob);
+        final params = {
+          'correctOrientation': true,
+          'mediaType': context['navigator']['camera']['MediaType']['PICTURE'],
+          'encodingType': context['Camera']['EncodingType']['JPEG'],
+          'destinationType': context['Camera']['DestinationType']['FILE_URI'],
+          'sourceType': take
+              ? context['Camera']['PictureSourceType']['CAMERA']
+              : context['Camera']['PictureSourceType']['PHOTOLIBRARY']
+        };
+        context['navigator']['camera'].callMethod('getPicture', [
+          (uri) async {
+            try {
+              final blob = await readAsBlob(uri, CONTENT_TYPE);
+              _logger.fine(() => "Get photo data: ${blob}");
+              _onChoose.complete(blob);
+            } catch (ex) {
+              _onChoose.completeError("Failed to read photo data: ${ex}");
+            }
           },
           (error) {
             _logger.fine("Failed to get photo: ${error}");
             _onChoose.completeError(error);
-          }
+          },
+          new JsObject.jsify(params)
         ]);
       } else {
         final file = await Dialog.chooseFile();
@@ -133,28 +136,10 @@ class PhotoShop {
       _original = new Completer();
       try {
         final blob = await photo;
-        final reader = new JsObject(context['FileReader'], []);
-        reader['onloadend'] = (event) {
-          try {
-            final arrayBuffer = reader['result'];
-            final array = new JsObject(context['Uint8Array'], [arrayBuffer]);
-            _logger.finest(() => "Reading image file data: ${arrayBuffer} => ${array}");
-            final list = new List<int>.generate(array['length'], (index) => array[index]);
-            _logger.finest(() => "Decoding image data: ${list.length}");
-            final image = decodeImage(list);
-            _logger.finest(() => "Decoded image: ${image}");
-            _original.complete(image);
-          } catch (ex) {
-            _logger.warning("Failed to decode photo image: ${ex}");
-            _original.completeError(ex);
-          }
-        };
-        reader['onerror'] = (event) {
-          final error = reader['error'];
-          _logger.warning("Failed to decode photo image: ${error}");
-          _original.completeError(error);
-        };
-        reader.callMethod('readAsArrayBuffer', [blob]);
+        _logger.finer(() => "Decoding photo data: ${blob}");
+        final image = decodeImage(await readAsList(blob));
+        _logger.finer(() => "Decoded photo image: ${image}");
+        _original.complete(image);
       } catch (ex) {
         _logger.warning("Failed to decode photo image: ${ex}");
         _original.completeError(ex);
@@ -167,8 +152,9 @@ class PhotoShop {
     } else {
       width = (original.width * maxSize / original.height).round();
     }
+    _logger.finest(() => "Resizing photo image: ${width}");
     final resized = copyResize(original, width);
     final data = encodeJpg(resized);
-    return new Blob([data]);
+    return new Blob([data], CONTENT_TYPE);
   }
 }
