cdac8197b9029921b44ed95d659245721678a39c
Kunio Sawatani <devel@fathens.org>
2015-09-18 16:53:30 +0900

    dart ディレクトリに移動

--- travis-scripts/dart.sh
+++ travis-scripts/dart.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -eu
 
+cd dart
+
 brew tap dart-lang/dart
 brew install dart
 
 gem install compass
-cat pubspec.yaml | awk '
-	{print $0}
-	/- sass/ { print "    executable: "$(type sass | awk '{print $NF}') }
-' > pubspec.yaml.tmp
-mv -vf pubspec.yaml.tmp pubspec.yaml
 
 cd dart
 pub get
