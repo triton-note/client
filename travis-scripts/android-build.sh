edbc1ed26cf3126203dda516b82b54a941b99526
Kunio Sawatani <devel@fathens.org>
2015-09-18 22:42:45 +0900

    sbt のインストール

--- travis-scripts/android-build.sh
+++ travis-scripts/android-build.sh
@@ -2,10 +2,18 @@
 set -eu
 
 echo
-echo "Building Android..."
+
+java -version
+brew install sbt
 
 if [ "$BUILD_MODE" == "release" ]
 then
+	echo "Building Android (release mode)..."
 else
+	echo "Building Android (test mode)..."
+	if [ "$BUILD_MODE" == "debug" ]
+	then
+		echo "Deploying Android (debug mode)..."
+	fi
 fi
 
--- travis-scripts/ios-build.sh
+++ travis-scripts/ios-build.sh
@@ -2,10 +2,15 @@
 set -eu
 
 echo
-echo "Building iOS..."
 
 if [ "$BUILD_MODE" == "release" ]
 then
+	echo "Building iOS (release mode)..."
 else
+	echo "Building iOS (test mode)..."
+	if [ "$BUILD_MODE" == "debug" ]
+	then
+		echo "Deploying iOS (debug mode)..."
+	fi
 fi
 
