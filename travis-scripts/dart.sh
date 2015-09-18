768bcd9dc8645a48423090586814566d2b8651e2
Kunio Sawatani <devel@fathens.org>
2015-09-18 21:09:20 +0900

    type コマンドの使い方を修正

--- travis-scripts/dart.sh
+++ travis-scripts/dart.sh
@@ -7,9 +7,11 @@ brew tap dart-lang/dart
 brew install dart
 
 gem install compass
-cat pubspec.yaml | awk '
-	{print $0}
-	/- sass/ { print "    executable: "system("type sass | awk '{print $NF}'") }
+cat pubspec.yaml | awk ' {print $0}
+	/- sass/ {
+		print "    executable: "
+		system("type -p sass")
+	}
 ' > pubspec.yaml.tmp
 mv -vf pubspec.yaml.tmp pubspec.yaml
 echo "Using pubspec.yaml"
