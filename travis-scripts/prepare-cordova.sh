ecc021780a243a9cef1ee19eb77336d1acbad6c7
Kunio Sawatani <devel@fathens.org>
2015-09-18 20:50:11 +0900

    ブランチ名で RELEASE と DEBUG を分ける

--- travis-scripts/prepare-cordova.sh
+++ travis-scripts/prepare-cordova.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -eu
 
-npm install -g cordova ionic
+. $(dirname $0)/check-mode.sh
 
+npm install cordova ionic
+
+if [ "$RELEASE" == "true" ]
+then
+	export FACEBOOK_APP_ID="$FACEBOOK_APP_ID_RELEASE"
+else
+	export FACEBOOK_APP_ID="$FACEBOOK_APP_ID_DEBUG"
+fi
+echo "$FACEBOOK_APP_ID"
 ./reinstall_plugins.sh
-
-cordova prepare
