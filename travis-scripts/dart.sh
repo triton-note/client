86ae91c9391060c06954a10443602ff3a17cdd4d
Kunio Sawatani <devel@fathens.org>
2015-09-24 08:58:34 +0900

    pubspec.yaml への sass のパス記入は不要
    PATH にちゃんと通っているようなので削除してみました。

--- travis-scripts/dart.sh
+++ travis-scripts/dart.sh
@@ -3,15 +3,5 @@ set -eu
 
 cd dart
 
-cat pubspec.yaml | awk ' {print $0}
-	/- sass/ {
-		"type -p sass" | getline path
-		print "    executable: "path
-	}
-' > pubspec.yaml.tmp
-mv -vf pubspec.yaml.tmp pubspec.yaml
-echo "Using pubspec.yaml"
-cat pubspec.yaml
-
 pub get
 pub build
