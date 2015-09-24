61167ac95bf56e5734f8f9b07226bd3bfd527aa2
Kunio Sawatani <devel@fathens.org>
2015-09-24 19:58:23 +0900

    TRACK_NAME を引数でもらう

--- travis-scripts/android-deploy/run.sh
+++ travis-scripts/android-deploy/run.sh
@@ -3,15 +3,13 @@ set -eu
 
 cd $(dirname $0)
 
-if [ ! -z "$ANDROID_GOOGLEPLAY_TRACK_NAME" ]
-then
-	echo "Deploying Android for ${ANDROID_GOOGLEPLAY_TRACK_NAME}..."
+ANDROID_GOOGLEPLAY_TRACK_NAME=$1
+echo "Deploying Android for ${ANDROID_GOOGLEPLAY_TRACK_NAME}..."
 
-	export ANDROID_GOOGLEPLAY_SERVICE_ACCOUNT_KEY_FILE_PATH="key.p12"
-	export ANDROID_GOOGLEPLAY_APK_FILE_PATH="${TRAVIS_BUILD_DIR}/platforms/android/ant-build/CordovaApp-release.apk"
-	
-	echo $ANDROID_GOOGLEPLAY_SERVICE_ACCOUNT_KEY_BASE64 | base64 -D > "$ANDROID_GOOGLEPLAY_SERVICE_ACCOUNT_KEY_FILE_PATH"
-	
-	export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-	sbt run
-fi
+export ANDROID_GOOGLEPLAY_SERVICE_ACCOUNT_KEY_FILE_PATH="key.p12"
+export ANDROID_GOOGLEPLAY_APK_FILE_PATH="${TRAVIS_BUILD_DIR}/platforms/android/ant-build/CordovaApp-release.apk"
+
+echo $ANDROID_GOOGLEPLAY_SERVICE_ACCOUNT_KEY_BASE64 | base64 -D > "$ANDROID_GOOGLEPLAY_SERVICE_ACCOUNT_KEY_FILE_PATH"
+
+export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
+sbt run
