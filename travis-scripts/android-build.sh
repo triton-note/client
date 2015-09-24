656b421bb5d95a0738f2aab22046244dfd6216cc
Kunio Sawatani <devel@fathens.org>
2015-09-24 11:47:52 +0900

    Android SDK の update を android-build.sh に移動

--- travis-scripts/android-build.sh
+++ travis-scripts/android-build.sh
@@ -10,6 +10,19 @@ key.store.password=$ANDROID_KEYSTORE_PASSWORD
 key.alias.password=$ANDROID_KEYSTORE_ALIAS_PASSWORD
 EOF
 
+update() {
+	echo y | android update sdk --no-ui --filter $1 || exit 1
+}
+
+echo <<EOF | while read name; do update $name; done
+tools
+platform-tools
+android-22
+addon-google_apis-google-22
+extra-google-m2repository
+extra-android-support
+EOF
+
 echo "Building Android..."
 cordova build android --release
 
