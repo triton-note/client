54028491528fbbfb00ccdd1cb38cadee79d1733a
Kunio Sawatani <devel@fathens.org>
2015-09-25 10:36:21 +0900

    track_name の判定が逆

--- travis-scripts/android-deploy.sh
+++ travis-scripts/android-deploy.sh
@@ -8,7 +8,8 @@ case "$BUILD_MODE" in
 "release") track_name=production;;
 "debug")   track_name=alpha;;
 esac
-if [ -z "$track_name" ]
+
+if [ ! -z "$track_name" ]
 then
 	git clone https://github.com/sawatani/CI-STEP-Deploy-GooglePlay.git android-deploy
 	./android-deploy/run.sh $track_name $(find $target_dir/build/ -name '*-release.apk')
