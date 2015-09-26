5a0c6e84a628eca33e643eb804f805f45c9cea7e
Kunio Sawatani <devel@fathens.org>
2015-09-26 16:37:46 +0900

    iOS のビルドを Shenzhen から Gym に変更

--- travis-scripts/ios-build.sh
+++ travis-scripts/ios-build.sh
@@ -6,7 +6,23 @@ cd "$(dirname $0)"
 ./ios-prepare-import-keychain.sh
 
 case "$BUILD_MODE" in
-"release") ./ios-build-release.sh;;
-"debug") ./ios-build-adhoc.sh;;
+"release") TARGET="Release";;
+"debug") TARGET="AdHoc";;
 esac
 
+cd "$(dirname $0)/../platforms/ios"
+
+echo
+echo "Building iOS for ${TARGET} on $(pwd)"
+
+export GYM_CLEAN=true
+export GYM_SDK="9.0"
+export GYM_SCHEME="$IOS_APPNAME"
+export GYM_INCLUDE_BITCODE=false
+export GYM_CONFIGURATION="Release"
+export GYM_CODE_SIGNING_IDENTITY="$IOS_DISTRIBUTION_CERTIFICATE_COMMON_NAME"
+export GYM_OUTPUT_DIRECTORY="./build"
+export GYM_DESTINATION="Distribution/${TARGET}"
+export GYM_PROVISIONING_PROFILE_PATH="MobileProvisionings/${IOS_APPNAME}_${TARGET}.mobileprovision"
+
+gym
