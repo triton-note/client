d2a529cc375b8133f9d4248ab490403f31b2da5b
Kunio Sawatani <devel@fathens.org>
2015-09-27 10:59:30 +0900

    case 文を修正

--- travis-scripts/cordova-prepare.sh
+++ travis-scripts/cordova-prepare.sh
@@ -3,8 +3,7 @@ set -eu
 
 case "$BUILD_MODE" in
 "release") export FACEBOOK_APP_ID="$FACEBOOK_APP_ID_RELEASE";;
-"beta")
-"debug") export FACEBOOK_APP_ID="$FACEBOOK_APP_ID_DEBUG";;
+"beta" | "debug") export FACEBOOK_APP_ID="$FACEBOOK_APP_ID_DEBUG";;
 esac
 ./reinstall_plugins.sh
 
