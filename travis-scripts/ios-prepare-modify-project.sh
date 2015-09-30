8b5e99742bd736b72236c9407077734619ddd7ac
Kunio Sawatani <devel@fathens.org>
2015-09-30 15:28:43 +0900

    Fabric.framework/run の BuildPhase を追加

--- travis-scripts/ios-prepare-modify-project.sh
+++ travis-scripts/ios-prepare-modify-project.sh
@@ -6,6 +6,14 @@ cd "$(dirname $0)/../platforms/ios"
 cat <<EOF | ruby
 require 'xcodeproj'
 
+def append_script(project, script)
+	project.targets.each do |target|
+		phase = target.new_shell_script_build_phase "Fabric"
+		phase.shell_script = script
+		target.shell_script_build_phases.push phase
+	end
+end
+
 def build_settings(project, params)
 	project.targets.each do |target|
 		target.build_configurations.each do |conf|
@@ -22,6 +30,7 @@ build_settings(project,
 	"ENABLE_BITCODE" => "NO",
 	"PROVISIONING_PROFILE" => "\$(PROFILE_UDID)"
 )
+append_script(project, "./Pods/Fabric/Fabric.framework/run $FABRIC_API_KEY $FABRIC_BUILD_SECRET")
 project.save
 EOF
 
