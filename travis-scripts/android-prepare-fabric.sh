1b86da8cc4b0c3f69aaa2df5ad4f6b699a6ef11b
Kunio Sawatani <devel@fathens.org>
2015-09-30 10:26:38 +0900

    build.grade は awk で編集

--- travis-scripts/android-prepare-fabric.sh
+++ travis-scripts/android-prepare-fabric.sh
@@ -3,20 +3,33 @@ set -eu
 
 cd "$(dirname $0)/../platforms/android"
 
-cat <<EOF | patch -p 0 build.gradle
-22a23
-> apply plugin: 'io.fabric'
-26a28
->         maven { url 'https://maven.fabric.io/public' }
-35a38
->             classpath 'io.fabric.tools:gradle:1.+'
-50a54
->     maven { url 'https://maven.fabric.io/public' }
-244a249,251
->     compile('com.crashlytics.sdk.android:crashlytics:2.5.2@aar') {
->       transitive = true
->     }
-EOF
+file=build.gradle
+cat "$file" | awk '
+	{
+		print $0
+		if (compile == 1) {
+			sub("[^ ].*", "compile('\''com.crashlytics.sdk.android:crashlytics:2.5.2@aar'\'') { transitive = true }")
+			print $0
+			compile=0
+		}
+	}
+	/apply plugin:/ {
+		sub("[^ ].*", "apply plugin: '\''io.fabric'\''")
+		print $0
+	}
+	/mavenCentral/ {
+		sub("[^ ].*", "maven { url '\''https://maven.fabric.io/public'\'' }")
+		print $0
+	}
+	/classpath/ {
+		sub("[^ ].*", "classpath '\''io.fabric.tools:gradle:1.+'\''")
+		print $0
+	}
+	/^dependencies / {
+		compile=1
+	}
+' > "${file}.tmp"
+mv -vf "${file}.tmp" "$file"
 
 cat <<EOF > fabric.properties
 apiSecret=$FABRIC_BUILD_SECRET
