761a2d2f1ff2eb4f4862a5e1c1cd0229ecc77f41
Kunio Sawatani <devel@fathens.org>
2015-10-02 20:33:51 +0900

    Facebook の Plugin も削除

--- reinstall_plugins.sh
+++ reinstall_plugins.sh
@@ -17,10 +17,6 @@ cordova plugin add cordova-plugin-geolocation
 cordova plugin add cordova-plugin-whitelist
 
 cordova plugin add phonegap-plugin-push
-#cordova plugin add https://github.com/sarriaroman/FabricPlugin
-
-# Facebook connect
-cordova plugin add https://github.com/Wizcorp/phonegap-facebook-plugin.git --variable APP_ID="$FACEBOOK_APP_ID" --variable APP_NAME="$FACEBOOK_APP_NAME"
 
 # Customized org.apache.cordova.file for GOOGLE_PHOTOS
 cordova plugin add https://github.com/sawatani/Cordova-plugin-file.git#GooglePhotos
