5187f1b587971c41d1a380a722f110cc7ab8493a
Kunio Sawatani <devel@fathens.org>
2015-10-02 18:01:06 +0900

    S3 へのキャッシュに aws s3 sync を使う

--- build-scripts/cache.sh
+++ build-scripts/cache.sh
@@ -3,29 +3,23 @@ set -eu
 
 action=$1
 folder=$2
+name=$3
+
+setup() {
+	sudo pip install awscli --upgrade
+
+	export AWS_ACCESS_KEY_ID=$S3_CACHE_ACCESS_KEY
+	export AWS_SECRET_ACCESS_KEY=$S3_CACHE_SECRET_KEY
+}
 
 load() {
-	brew install s3cmd
-
-	cat <<EOF | s3cmd --configure
-$S3_CACHE_ACCESS_KEY
-$S3_CACHE_SECRET_KEY
-
-
-
-
-
-n
-y
-EOF
-
-	s3cmd get s3://cache-build/${folder}/node_modules.tar.bz2
-	tar jxf node_modules.tar.bz2 > /dev/null
+	mkdir -vp $name
+	aws s3 sync s3://cache-build/$folder/$name $name --delete
 }
 
 save() {
-	tar jcf node_modules.tar.bz2 node_modules > /dev/null
-	s3cmd put node_modules.tar.bz2 s3://cache-build/${folder}/node_modules.tar.bz2
+	aws s3 sync $name s3://cache-build/$folder/$name --delete
 }
 
+setup
 $action
