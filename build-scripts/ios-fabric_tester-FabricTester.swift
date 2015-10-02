3a4cd98ef0b72d460bffb0348df42a82bea115a2
Kunio Sawatani <devel@fathens.org>
2015-10-02 13:22:15 +0900

    iOS の Crashlytics Custom Log のテストに throwException を追加

--- build-scripts/ios-fabric_tester-FabricTester.swift
+++ build-scripts/ios-fabric_tester-FabricTester.swift
@@ -5,7 +5,8 @@ class FabricTester: NSObject {
         dispatch_after(
             dispatch_time(DISPATCH_TIME_NOW, Int64(5 * Double(NSEC_PER_SEC))),
             dispatch_get_main_queue(), {
-            CLSLogv("Gondra at: %@", getVaList([NSDate()]))
+                CLSLogv("Gondra at: %@", getVaList([NSDate()]))
+                Crashlytics.sharedInstance().throwException()
         })
     }
 }
