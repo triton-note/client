4898408e96ee9100a6562e58ad44044117a8c87d
Kunio Sawatani <devel@fathens.org>
2015-10-03 18:00:39 +0900

    iOS の Fabric テストコードで CLS_LOG ではなく setValue をテスト

--- build-scripts/ios-fabric_tester-FabricTester.m
+++ build-scripts/ios-fabric_tester-FabricTester.m
@@ -9,7 +9,7 @@
 - (void) doTest {
     @autoreleasepool {
         [NSThread sleepForTimeInterval:5];
-        CLS_LOG(@"Gondra on %@", [NSDate date]);
+        [CrashlyticsKit setValue:@"Use CrashlyticsKit" forKey:@"VER"];
         [CrashlyticsKit throwException];
     }
 }
