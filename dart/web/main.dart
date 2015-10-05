fd15e91dc5a62c1cfa941dc38a76ac9b2eeb80c0
Kunio Sawatani <devel@fathens.org>
2015-10-05 17:55:55 +0900

    Fabric.crashlytics を Crashlytics と Answers に分離

--- dart/web/main.dart
+++ dart/web/main.dart
@@ -77,8 +77,8 @@ void main() {
 
   initPolymer().then((zone) => Polymer.onReady.then((_) {
         onDeviceReady((event) {
-          //window.fabric.Answers.sendLogIn();
-          context['plugin']['clashlytics'].callMethod('logException', ["Fire from Dart"]);
+          context['plugin']['Fabric']['Answers'].callMethod('sendLogin', [new JsObject.jsify({"method", "Cognito"})]);
+          context['plugin']['Fabric']['Clashlytics'].callMethod('logException', ["Fire from Dart"]);
           applicationFactory().addModule(new AppModule()).run();
         });
       }));
