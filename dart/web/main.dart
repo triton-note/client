a71bf0b26f56b6045ca35edd5dbbc36a0a94b42e
Kunio Sawatani <devel@fathens.org>
2015-10-15 21:14:17 +0900

    Crashlytics Plugin のテスト用コードを削除

--- dart/web/main.dart
+++ dart/web/main.dart
@@ -90,25 +90,21 @@ void main() {
     });
 
   try {
-    window.alert('Pop on main');
-    document.on['deviceready'].listen((event) {
-      window.alert('Pop onDeviceReady: ${event}');
+    onDeviceReady((event) {
       try {
         initPolymer().then((zone) {
           zone.run(() {
-            window.alert('Pop initPolymer');
             Polymer.onReady.then((_) {
-              window.alert('Pop Polymer.onReady');
-
               applicationFactory().addModule(new AppModule()).run();
             });
           });
         });
       } catch (ex) {
-        FabricCrashlytics.logException("$ex");
+        FabricCrashlytics.crash("$ex");
       }
     });
   } catch (ex) {
     window.alert("Error ${ex}");
+    FabricCrashlytics.crash("$ex");
   }
 }
