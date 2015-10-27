490fedb819e7cdf2178fc60b465133af42d0894b
Kunio Sawatani <devel@fathens.org>
2015-10-27 22:08:58 +0900

    ビルド処理の最初のインストールまでを start.sh に実装

---
+++ build-scripts/cache.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import boto3
+import tarfile
+import io
+
+def getObject(name):
+    s3 = boto3.resource('s3')
+    folder = os.environ['PROJECT_FOLDER']
+    bucket = s3.Bucket(os.environ['AWS_S3_BUCKET'])
+    filename = name + '.tar.bz2'
+    return (bucket.Object(folder + "/" + filename), filename)
+
+def mkdirs(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+def load(name):
+    (obj, filename) = getObject(name)
+    print('Loading', obj, 'to', filename)
+    mkdirs(filename)
+    file = open(filename, mode='wb')
+    try:
+        file.write(obj.get()['Body'].read())
+        tar = tarfile.open(mode='r:bz2', name=filename)
+        for member in tar.getmembers():
+            if member.isfile():
+                mkdirs(member.name)
+                src = tar.extractfile(member)
+                dst = open(member.name, mode='wb')
+                dst.write(src.read())
+                dst.close()
+    finally:
+        file.close()
+        os.remove(filename)
+
+def save(name):
+    (obj, filename) = getObject(name)
+    print('Saving', filename, 'to', obj)
+    tar = tarfile.open(mode='w:bz2', name=filename)
+    tar.add(name)
+    tar.close()
+    file = open(filename, mode='rb')
+    try:
+        obj.put(Body=file.read())
+    finally:
+        file.close()
+        os.remove(filename)
+
+if __name__ == "__main__":
+    action = sys.argv[1]
+    if len(sys.argv) < 3:
+        list = ['node_modules']
+    else:
+        list = sys.argv[2:]
+
+    print(action, list)
+    for name in list:
+        if action == "load":
+            load(name)
+        elif action == "save":
+            save(name)
---
+++ build-scripts/start.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -eu
+
+echo "################"
+echo "# Start building"
+echo "IS_CI=${IS_CI:-}"
+echo "BUILD_NUM=$BUILD_NUM"
+echo "BUILD_MODE=$BUILD_MODE"
+echo "PROJECT_REPO_SLUG=$PROJECT_REPO_SLUG"
+echo "RELEASE_NOTE_PATH=$RELEASE_NOTE_PATH"
+echo "PLATFORM=$PLATFORM"
+
+echo "################"
+echo "# Using Python3"
+
+brew update
+brew install python3
+
+[ -z "$(type pip 2> /dev/null)" ] && sudo easy_install pip
+sudo pip install virtualenvwrapper
+
+export WORKON_HOME=~/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+
+mkvirtualenv --python=/usr/local/bin/python3 V3
+workon V3
+
+pip install boto3
+
+echo "################"
+echo "# Install tools"
+
+build-scripts/cache.py load build-scripts/persistent
+build-scripts/cache.py load
+
+cat<<EOF > $HOME/.gemrc
+gem: --no-document
+EOF
+
+brew tap dart-lang/dart && brew install dart
+sudo gem install compass
+npm install
+export PATH="$PATH:$(pwd)/node_modules/.bin"
+
+build-scripts/cache.py save
+
+#### Start building
+build-scripts/main.py
