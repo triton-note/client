8eac05899b6c31a7d5a2b7b74d1c6bf7b05809dc
Kunio Sawatani <devel@fathens.org>
2015-10-28 00:02:15 +0900

    dart/web/settings.yaml を作成するところまで実装

---
+++ build-scripts/dart.py
@@ -0,0 +1,26 @@
+import os
+import config_variables
+
+config = config_variables.Config()
+
+def mkdirs(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+def write_settings():
+    target = os.path.join('dart', 'web', 'settings.yaml')
+    mkdirs(target)
+    file = open(target, mode='w')
+    try:
+        def put(name, key):
+            value = config.get(key)
+            file.write('%s: %s\n'%(name, value))
+        put('awsRegion: ', 'aws.REGION')
+        put('cognitoPoolId: ', 'aws.COGNITO_POOL_ID')
+        put('s3Bucket: ', 'aws.S3_BUCKET')
+    finally:
+        file.close()
+
+def all():
+    write_settings()
---
+++ build-scripts/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import dart
+
+if __name__ == "__main__":
+    dart.all()
