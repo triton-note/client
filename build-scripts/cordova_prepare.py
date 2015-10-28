2ddc1d3f4c935bf56954d4ad4e6f483ae046df68
Kunio Sawatani <devel@fathens.org>
2015-10-28 17:38:22 +0900

    cordova-prepare.sh の内容を main.py に移行

---
+++ build-scripts/cordova_prepare.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import shell
+
+def environment_variables():
+    print('Setting environment variables')
+
+def execute():
+    shell.mkdirs('plugins')
+    shell.cmd('cordova prepare %s' % os.enriron['PLATFORM'])
+    shell.cmd('ionic resources')
+
+def all():
+    environment_variables()
+    execute()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "env":
+            environment_variables()
+    execute()
--- build-scripts/main.py
+++ build-scripts/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import cordova_prepare
 import dart
 
 if __name__ == "__main__":
     dart.all()
+    cordova_prepare.all()
