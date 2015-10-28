91bc7d20c7a02d0523b7d44e1b923a738c7379a8
Kunio Sawatani <devel@fathens.org>
2015-10-28 18:50:23 +0900

    各プラットフォームのビルドを作成開始

---
+++ build-scripts/ios_build.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from config import Config
+import shell
+
+def all():
+    print('Building iOS')
+
+if __name__ == "__main__":
+    Config.load()
--- build-scripts/main.py
+++ build-scripts/main.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
 from config import Config
+import android_build
 import cordova_prepare
 import dart
+import ios_build
+
+def platform_build():
+    PLATFORM = os.environ['PLATFORM']
+    platforms = {
+                 'android': android_build,
+                 'ios': ios_build
+                 }
+    platform = platforms.get(PLATFORM)
+    if platform:
+        platform.all()
+    else:
+        sys.exit("Unsupported platform: %s" % PLATFORM)
 
 if __name__ == "__main__":
     Config.load()
 
     dart.all()
     cordova_prepare.all()
+    platform_build()
