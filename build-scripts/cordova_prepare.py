065c9da61bcdb16ee7021e32cf126c273bf7a47c
Kunio Sawatani <devel@fathens.org>
2015-10-30 00:07:27 +0900

    cordova_prepare をコマンドとして充実

--- build-scripts/cordova_prepare.py
+++ build-scripts/cordova_prepare.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+from optparse import OptionParser
 import os
+import shutil
 import sys
 
 from config import Config
 import shell
+
 
 def environment_variables():
     print('Setting environment variables')
@@ -13,6 +16,11 @@ def environment_variables():
     os.environ['FABRIC_API_KEY'] = Config.get('fabric.API_KEY')
     os.environ['FABRIC_BUILD_SECRET'] = Config.get('fabric.BUILD_SECRET')
     os.environ['CRASHLYTICS_GROUPS'] = Config.get('fabric.CRASHLYTICS_GROUPS')
+
+def cleanup():
+    print('Clean Up')
+    shutil.rmtree('plugins')
+    shutil.rmtree('platforms')
 
 def execute():
     shell.mkdirs('plugins')
@@ -26,7 +34,16 @@ def all():
 if __name__ == "__main__":
     shell.on_root(sys.argv[0])
     Config.load()
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "env":
-            environment_variables()
-    execute()
+
+    opt_parser = OptionParser()
+    opt_parser.add_option('-e', '--env', help='set environment variables', action="store_true", dest='env', default=False)
+    opt_parser.add_option('-c', '--cleanup', help='cleanup before execute', action="store_true", dest='cleanup', default=False)
+    opt_parser.add_option('-d', '--dry-run', help='no execution', action="store_false", dest='execute', default=True)
+    options, args = opt_parser.parse_args()
+
+    if options.env:
+        environment_variables()
+    if options.cleanup:
+        cleanup()
+    if options.execute:
+        execute()
