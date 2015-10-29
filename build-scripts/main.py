b920016d2bfefe883d7b9859909a1c9e30816746
Kunio Sawatani <devel@fathens.org>
2015-10-29 15:35:27 +0900

    android_build と ios_build を改名して直接実行

--- build-scripts/main.py
+++ build-scripts/main.py
@@ -4,26 +4,14 @@ import os
 import sys
 
 from config import Config
-import android_build
+import android
 import cordova_prepare
 import dart
-import ios_build
-
-def platform_build():
-    PLATFORM = os.environ['PLATFORM']
-    platforms = {
-                 'android': android_build,
-                 'ios': ios_build
-                 }
-    platform = platforms.get(PLATFORM)
-    if platform:
-        platform.all()
-    else:
-        sys.exit("Unsupported platform: %s" % PLATFORM)
+import ios
 
 if __name__ == "__main__":
     Config.load()
 
     dart.all()
     cordova_prepare.all()
-    platform_build()
+    globals()[os.environ['PLATFORM']].all()
