7b930e2f09cd4385892e035d33d5b50ce6500a02
Kunio Sawatani <devel@fathens.org>
2015-10-30 16:11:34 +0900

    start.sh を構造化

--- build-scripts/start.sh
+++ build-scripts/start.sh
@@ -1,43 +1,50 @@
 #!/bin/sh
 
-echo "################"
+echo "################################"
 echo "# Start building"
 echo "BUILD_NUM=$BUILD_NUM"
 echo "BUILD_MODE=$BUILD_MODE"
 echo "PROJECT_REPO_SLUG=$PROJECT_REPO_SLUG"
-echo "RELEASE_NOTE_PATH=$RELEASE_NOTE_PATH"
-echo "PLATFORM=$PLATFORM"
 
-echo "################"
-echo "# Using Python3"
+into_python() {
+    echo "################################"
+    echo "# Using Python3"
+    
+    [ -z "$(type python3 2> /dev/null)" ] && (brew update && brew install python3)
+    
+    [ -z "$(type pip 2> /dev/null)" ] && sudo easy_install pip
+    [ -z "$(type virtualenv 2> /dev/null)" ] && sudo pip install virtualenv
+    
+    [ -f .v3/bin/activate ] || virtualenv --python=$(type -p python3) .v3
+    [ "${VIRTUAL_ENV}x" == "x" ] && source .v3/bin/activate
+}
 
-brew update
-brew install python3
+install() {
+    echo "################################"
+    echo "# Install tools"
+    
+    pip install boto3
+    build-scripts/cache.py load build-scripts/persistent
+    build-scripts/cache.py load
+    
+    echo 'gem: --no-document' > $HOME/.gemrc
+    
+    pip install pyyaml lxml
+    brew tap dart-lang/dart && brew install dart
+    sudo gem install compass
+    npm install
+    export PATH="$PATH:$(pwd)/node_modules/.bin"
+    
+    build-scripts/cache.py save
+}
 
-[ -z "$(type pip 2> /dev/null)" ] && sudo easy_install pip
-sudo pip install virtualenv
+build() {
+    echo "################################"
+    echo "# Building all"
+    
+    build-scripts/main.py
+}
 
-virtualenv --python=/usr/local/bin/python3 .v3
-source .v3/bin/activate
-
-pip install boto3 pyyaml lxml
-
-echo "################"
-echo "# Install tools"
-
-build-scripts/cache.py load build-scripts/persistent
-build-scripts/cache.py load
-
-cat<<EOF > $HOME/.gemrc
-gem: --no-document
-EOF
-
-brew tap dart-lang/dart && brew install dart
-sudo gem install compass
-npm install
-export PATH="$PATH:$(pwd)/node_modules/.bin"
-
-build-scripts/cache.py save
-
-#### Start building
-build-scripts/main.py
+time into_python
+time install
+time build
