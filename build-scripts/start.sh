1d8c66ae8f6e82af0c3493a495dd49a5336eae76
Kunio Sawatani <devel@fathens.org>
2015-11-04 19:52:41 +0900

    Python3 のインストールを travis.yml に移管

--- build-scripts/start.sh
+++ build-scripts/start.sh
@@ -5,18 +5,6 @@ echo "# Start building"
 echo "BUILD_NUM=$BUILD_NUM"
 echo "PROJECT_REPO_SLUG=$PROJECT_REPO_SLUG"
 
-into_python() {
-    echo "################################"
-    echo "# Using Python3"
-    
-    [ -z "$(type python3 2> /dev/null)" ] && brew install python3
-    pip3 --version
-    [ -z "$(type virtualenv 2> /dev/null)" ] && sudo pip3 install virtualenv
-    
-    [ -f .v3/bin/activate ] || virtualenv --python=$(type -p python3) .v3
-    [ -z "${VIRTUAL_ENV:-}" ] && source .v3/bin/activate
-}
-
 install() {
     echo "################################"
     echo "# Install tools"
@@ -43,7 +31,5 @@ build() {
     build-scripts/main.py
 }
 
-brew update || brew update
-time into_python
 time install
 time build
