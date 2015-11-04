901920bf83b9b8c323ff068d8c90bcbbe6bb7777
Kunio Sawatani <devel@fathens.org>
2015-11-04 17:42:54 +0900

    Revert "deployed タグの場合はビルドを進めない"
    This reverts commit d918b9df6f90fd2806f3203569678cdbe9c0e659.

--- build-scripts/main.py
+++ build-scripts/main.py
@@ -14,25 +14,21 @@ import shell
 
 
 if __name__ == "__main__":
+    shell.on_root()
     Config.init()
     GitHub.init()
 
-    if GitHub.is_deployed():
-        print('This is deployed tag')
-    else:
-        shell.on_root()
+    note = GitHub.release_note()
+    print('################ Release Note ################')
+    print(note)
+    print('################')
+    target = Config.script_file('.release_note')
+    with open(target, mode='w') as file:
+        file.write(note + '\n')
+    os.environ['RELEASE_NOTE_PATH'] = target
 
-        note = GitHub.release_note()
-        print('################ Release Note ################')
-        print(note)
-        print('################')
-        target = Config.script_file('.release_note')
-        with open(target, mode='w') as file:
-            file.write(note + '\n')
-        os.environ['RELEASE_NOTE_PATH'] = target
+    dart.all()
+    cordova_prepare.all()
+    globals()[Config.PLATFORM].all()
 
-        dart.all()
-        cordova_prepare.all()
-        globals()[Config.PLATFORM].all()
-
-        GitHub.put_tag()
+    GitHub.put_tag()
