a7ca0b1b81ce254d8abf2da99942792d48176e91
Kunio Sawatani <devel@fathens.org>
2015-11-13 19:29:55 +0900

    dart.rb の font と js のダウンロードを実装

--- fastlane/actions/dart.rb
+++ fastlane/actions/dart.rb
@@ -3,7 +3,7 @@ module Fastlane
     class DartAction < Action
       def self.run(params)
         install
-        Dir.chdir(Actions.lane_context[Actions::SharedValues::PROJECT_ROOT]) do
+        Dir.chdir(File.join(Actions.lane_context[Actions::SharedValues::PROJECT_ROOT], 'dart')) do
           write_settings
           index_download
           build
@@ -31,37 +31,6 @@ module Fastlane
       end
 
       def self.index_download
-        def download(url, dir)
-          def unique_name(base)
-            require "digest/md5"
-            names = [Digest::MD5.digest(base)]
-            m = /.*[^\w]([\w]+)$/.match base.split('?')[0]
-            if m != nil then
-              names << m[1]
-            end
-            "cached-#{names.join('.')}"
-          end
-          name = unique_name(url)
-          target = File.join(dir, name)
-          Dir.mkdir(dir)
-          retry_count = 3
-          begin
-            require 'open-uri'
-            puts "Downloading #{url} to #{target}"
-            open(target, 'wb') do |file|
-              open(url, 'rb') do |res|
-                file.write res.read
-              end
-            end
-          rescue
-            if 0 < --retry_count then
-              retry
-            else
-              raise
-            end
-          end
-        end
-
         require 'nokogiri'
         target = File.join('web', 'index.html')
         doc = File.open(target) do |file|
@@ -72,31 +41,71 @@ module Fastlane
           href = css['href']
           if /^https:\/\/fonts.googleapis.com\/css\?.*$/.match href then
             dir = File.join('web', 'styles', 'fonts')
-            filename = download(url, dir)
-            target = File.join(dir, filename)
-            
-            lines = File.open(target) do |file|
-              file.readlines              
-            end
-            
-            lines.each do |line|
-              m = /(^.*url\()(https:[^\)]+)(\).*)/.match line
-              if m != nil then
-                loaded = download(m[2], dir)
-                line = "#{m[1]}#{loaded}#{m[3]}"
+            filename = download(href, dir)
+
+            File.open(File.join(dir, filename), 'r+') do |file|
+              lines = file.readlines
+              file.seek(0)
+
+              lines.each do |line|
+                m = /(^.*url\()(https:[^\)]+)(\).*)/.match line
+                if m != nil then
+                  loaded = download(m[2], dir)
+                  line = "#{m[1]}#{loaded}#{m[3]}"
+                end
+                file.puts line
               end
+              file.flush
+              file.truncate(file.pos)
             end
-            
-            File.open(target) do |file|
-              file.write lines.join("\n")
-            end
-            css['href'] = filename
+
+            css['href'] = File.join('styles', 'fonts', filename)
+          end
+        end
+        
+        doc.xpath("//script[@type='text/javascript']").each do |js|
+          href = js['src']
+          if /^https:\/\/.*\.js$/.match(href) then
+            js['src'] = File.join('js', download(href, File.join('web', 'js')))
           end
         end
 
-        File.open(target) do |file|
+        File.open(target, 'w') do |file|
           file.write doc.to_html
         end
+      end
+
+      def self.download(url, dir)
+        require "digest/md5"
+        names = [Digest::MD5.hexdigest(url)]
+        m = /.*[^\w]([\w]+)$/.match url.split('?')[0]
+        if m != nil then
+          names << m[1]
+        end
+        name = "cached-#{names.join('.')}"
+        target = File.join(dir, name)
+        if !File.exist?(dir) then
+          Dir.mkdir(dir)
+        end
+        retry_count = 3
+        begin
+          require 'open-uri'
+          puts "Downloading #{url} to #{target}"
+          open(target, 'wb') do |file|
+            open(url, 'rb') do |res|
+              file.write res.read
+            end
+          end
+        rescue
+          puts "Error on downloading"
+          retry_count -= 1
+          if 0 < retry_count then
+            retry
+          else
+            raise
+          end
+        end
+        return name
       end
 
       def self.build
