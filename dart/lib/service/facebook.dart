a4fb3a476db124b12a7cf42fb54a614f480957b5
Kunio Sawatani <devel@fathens.org>
2015-11-25 16:21:35 +0900

    FBConnect の login や logout は失敗したら例外を投げる
    bool での false を返すのではなく例外を投げるようにしました。

--- dart/lib/service/facebook.dart
+++ dart/lib/service/facebook.dart
@@ -17,11 +17,9 @@ class FBConnect {
   static Future _call(String name, List args) {
     final completer = new Completer<String>();
     args.insert(0, (error, result) {
-      _logger.finest(() => 'Result of plugin.FBConnect.${name}: ${result}, error: ${error}');
       if (result != null) {
         result = JSON.decode(context['JSON'].callMethod('stringify', [result]));
       }
-
       if (error == null) {
         completer.complete(result);
       } else {
@@ -32,36 +30,21 @@ class FBConnect {
     return completer.future;
   }
 
-  static Future<bool> login() async {
-    try {
-      final token = await _call('login', []) as String;
-      final cred = await CognitoIdentity.joinFacebook(token);
-      _logger.finest(() => "Logged in: cognito id: ${cred.id}");
-      return cred.hasFacebook();
-    } catch (ex) {
-      _logger.warning(() => "Failed to connect to Facebook: ${ex}");
-      return false;
-    }
+  static Future<String> _login(List<String> perms) async {
+    final token = await _call('login', perms) as String;
+    final cred = await CognitoIdentity.joinFacebook(token);
+    if (!cred.hasFacebook()) throw "Failed to connect to Facebook.";
+    return token;
   }
 
-  static Future<bool> logout() async {
-    try {
-      await _call('logout', []);
-      final cred = await CognitoIdentity.dropFacebook();
-      _logger.finest(() => "Logged out: cognito id: ${cred.id}");
-      if (cred.hasFacebook()) {
-        _logger.warning(() => "Failed to disconnect from Facebook. Still connected.");
-        return false;
-      } else {
-        return true;
-      }
-    } catch (ex) {
-      _logger.warning(() => "Failed to disconnect from Facebook: ${ex}");
-      return false;
-    }
+  static Future<Null> logout() async {
+    await _call('logout', []);
+    final cred = await CognitoIdentity.dropFacebook();
+    if (cred.hasFacebook()) throw "Failed to disconnect from Facebook. Still connected.";
   }
 
-  static Future<String> grantPublish() => _call('login', ['publish_actions']);
+  static Future<String> login() => _login([]);
+  static Future<String> grantPublish() => _login(['publish_actions']);
   static Future<String> getName() => _call('getName', []);
   static Future<Map> getToken() => _call('getToken', []);
 }
@@ -87,47 +70,47 @@ class _FBSettings {
 class FBPublish {
   static final _logger = new Logger('FBPublish');
 
-  static publish(Report report) async {
+  static Future<String> publish(Report report) async {
     _logger.fine(() => "Publishing report: ${report.id}");
 
+    final token = await FBConnect.grantPublish();
     final cred = await CognitoIdentity.credential;
     final settings = await Settings;
     final fbSettings = await _FBSettings.load();
 
-    api(String name, Map info) {
-      final json = JSON.encode(info);
-      final urlen = Uri.encodeFull(json);
+    og(String name, Map info) {
+      final urlen = Uri.encodeFull(JSON.encode(info));
       final base64 = new Base64Encoder().convert(new AsciiEncoder().convert(urlen));
       return "https://api.fathens.org/triton-note/open_graph/${name}/${base64}";
     }
-    apiSpot() => api('spot', {
-          'region': settings.awsRegion,
-          'table_report': "${settings.appName}.REPORT",
-          'cognitoId': cred.id,
-          'reportId': report.id
-        });
-    apiReport() => api('catch_report', {
-          'region': settings.awsRegion,
-          'bucketName': settings.s3Bucket,
-          'urlTimeout': fbSettings.imageTimeout,
-          'table_report': "${settings.appName}.REPORT",
-          'table_catch': "${settings.appName}.CATCH",
-          'cognitoId': cred.id,
-          'reportId': report.id
-        });
 
     final params = {
       'fb:explicitly_shared': ['true'],
       'message': report.comment,
-      'place': apiSpot(),
-      fbSettings.objectName: apiReport(),
       "image[0][url]": report.photo.original.url,
-      "image[0][user_generated]": 'true'
+      "image[0][user_generated]": 'true',
+      'place': og('spot', {
+        'region': settings.awsRegion,
+        'table_report': "${settings.appName}.REPORT",
+        'cognitoId': cred.id,
+        'reportId': report.id
+      }),
+      fbSettings.objectName: og('catch_report', {
+        'region': settings.awsRegion,
+        'bucketName': settings.s3Bucket,
+        'urlTimeout': fbSettings.imageTimeout,
+        'table_report': "${settings.appName}.REPORT",
+        'table_catch': "${settings.appName}.CATCH",
+        'cognitoId': cred.id,
+        'reportId': report.id
+      })
     };
 
-    final token = await FBConnect.grantPublish();
-    final url = "${fbSettings.hostname}/me/${fbSettings.appName}:${fbSettings.actionName}?access_token=${token}";
-    final result = await new http.Client().post(url, body: params);
+    final url = "${fbSettings.hostname}/me/${fbSettings.appName}:${fbSettings.actionName}";
+    _logger.fine(() => "Posting to ${url}: ${params}");
+
+    final result = await new http.Client().post("${url}?access_token=${token}", body: params);
+    _logger.fine(() => "Result of posting to facebook: ${result}");
 
     if (result.statusCode % 100 != 2) {
       throw result.body;
@@ -138,6 +121,7 @@ class FBPublish {
       } else {
         final published = obj['id'];
         _logger.info(() => "Report(${report.id}) is published: ${published}");
+        return published;
       }
     }
   }
