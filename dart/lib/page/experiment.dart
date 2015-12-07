9a992cbcffc5da105bfcf381d3d91f396a8738f3
Kunio Sawatani <devel@fathens.org>
2015-12-07 12:56:17 +0900

    experiment に Facebook の login などは不要

--- dart/lib/page/experiment.dart
+++ dart/lib/page/experiment.dart
@@ -5,7 +5,6 @@ import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:logging/logging.dart';
 
-import 'package:triton_note/service/reports.dart';
 import 'package:triton_note/service/facebook.dart';
 import 'package:triton_note/util/fabric.dart';
 import 'package:triton_note/util/main_frame.dart';
@@ -20,49 +19,12 @@ final _logger = new Logger('ExperimentPage');
 class ExperimentPage extends MainFrame {
   ExperimentPage(Router router) : super(router) {}
 
-  fbLogin() async {
-    try {
-      final result = await FBConnect.login();
-      window.alert("login: ${result}");
-    } catch (ex) {
-      window.alert("login: Error: ${ex}");
-    }
-  }
-
-  fbLogout() async {
-    try {
-      final result = await FBConnect.logout();
-      window.alert("logout: ${result}");
-    } catch (ex) {
-      window.alert("logout: Error: ${ex}");
-    }
-  }
-
   fbToken() async {
     try {
       final result = await FBConnect.getToken();
       window.alert("getToken: ${result}");
     } catch (ex) {
       window.alert("getToken: Error: ${ex}");
-    }
-  }
-
-  fbGain() async {
-    try {
-      final result = await FBConnect.grantPublish();
-      window.alert("grantPublish: ${result}");
-    } catch (ex) {
-      window.alert("grantPublish: Error: ${ex}");
-    }
-  }
-
-  fbPublish() async {
-    try {
-      final pager = await Reports.paging;
-      final result = await FBPublish.publish(pager.list.first);
-      window.alert("publish: ${result}");
-    } catch (ex) {
-      window.alert("publish: Error: ${ex}");
     }
   }
 
