d36174749f656be22a1de2ed13a1e3ba2de0d607
Kunio Sawatani <devel@fathens.org>
2015-12-24 13:38:10 +0900

    addFish ボタン押下時に fish-name が空なら Blink

--- dart/lib/page/add_report.dart
+++ dart/lib/page/add_report.dart
@@ -25,6 +25,7 @@ import 'package:triton_note/service/reports.dart';
 import 'package:triton_note/service/geolocation.dart' as Geo;
 import 'package:triton_note/service/googlemaps_browser.dart';
 import 'package:triton_note/service/aws/s3file.dart';
+import 'package:triton_note/util/blinker.dart';
 import 'package:triton_note/util/enums.dart';
 import 'package:triton_note/util/main_frame.dart';
 import 'package:triton_note/util/getter_setter.dart';
@@ -180,11 +181,27 @@ class AddReportPage extends SubPage {
 
   String addingFishName;
 
+  _fishNameBlinkArea() => [root.querySelector('#catches .control .fish-name input')];
+  static const fishNameBlinkDuration = const Duration(milliseconds: 350);
+  static const fishNameBlinkUpDuration = const Duration(milliseconds: 100);
+  static const fishNameBlinkDownDuration = const Duration(milliseconds: 100);
+  static const fishNameBlinkFrames = const [
+    const {'background': "transparent"},
+    const {'background': "#fee"}
+  ];
+
   addFish() {
     if (addingFishName != null && addingFishName.isNotEmpty) {
       final fish = new Fishes.fromMap({'name': addingFishName, 'count': 1});
       addingFishName = null;
       report.fishes.add(fish);
+    } else {
+      final blinker = new Blinker(fishNameBlinkUpDuration, fishNameBlinkDownDuration,
+          [new BlinkTarget(new Getter(_fishNameBlinkArea), fishNameBlinkFrames)]);
+      blinker.start();
+      new Future.delayed(fishNameBlinkDuration, () {
+        blinker.stop();
+      });
     }
   }
 
